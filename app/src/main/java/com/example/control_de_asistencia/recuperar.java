b6005cbad44988343ed99e5b87113ac04c4a2213
Catherine <noodle256215@gmail.com>
2020-05-10 02:26:51 -0500

    Se acomodaron componentes
    Algunos componentes no se adaptaron adecuadamente, lo cual se observaba en el Emulador. Quedó solucionado.

--- app/src/main/java/com/example/control_de_asistencia/recuperar.java
+++ app/src/main/java/com/example/control_de_asistencia/recuperar.java
@@ -1,7 +1,9 @@
 package com.example.control_de_asistencia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class recuperar extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class recuperar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recuperar);
+    }
+    public  void Registro(View view){
+        Intent next = new Intent(this,registro.class);
+        startActivity(next);
     }
 }
