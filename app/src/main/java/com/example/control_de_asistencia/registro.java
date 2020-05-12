44a61a1e6cba44622bb581c62ec50a869129104c
Catherine <noodle256215@gmail.com>
2020-05-12 01:19:55 -0500

    Validación de campos del Registro

--- app/src/main/java/com/example/control_de_asistencia/registro.java
+++ app/src/main/java/com/example/control_de_asistencia/registro.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -17,6 +18,8 @@ public class registro extends AppCompatActivity {
     EditText et_user, et_email, et_passw;
     Button btn_registrar;
     ImageButton btn_info;
+    CheckBox cb_terminos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,7 @@ public class registro extends AppCompatActivity {
         et_passw = (EditText) findViewById(R.id.txt_password);
         btn_registrar= (Button) findViewById(R.id.btn_next);
         btn_info= (ImageButton) findViewById(R.id.btn_info);
+        cb_terminos=(CheckBox) findViewById(R.id.cb_terminos);
 
         btn_info.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,27 +42,44 @@ public class registro extends AppCompatActivity {
         btn_registrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Guardar(et_user.getText().toString(), et_email.getText().toString(), et_passw.getText().toString());
+                Guardar(et_user.getText().toString(), et_email.getText().toString(),
+                        et_passw.getText().toString(), cb_terminos.isChecked());
             }
         });
 
-
-
     }
-    public void Guardar(String user, String email, String passw){
+
+
+    public void Guardar(String user, String email, String passw, boolean cb){
         BaseHelper helper = new BaseHelper(this, "Demo", null, 1);
         SQLiteDatabase db = helper.getWritableDatabase();
-        try{
-            ContentValues c = new ContentValues();
-            c.put("USER", user);
-            c.put("EMAIL", email);
-            c.put("PASS", passw);
-            db.insert("PERSONA", null, c);
-            db.close();
-            Toast.makeText(this, "Registro insertado.", Toast.LENGTH_LONG).show();
-        } catch (Exception e){
-            Toast.makeText(this, "Error:"+e.getMessage(), Toast.LENGTH_LONG).show();
+
+        if(user.length()==0){
+            Toast.makeText(this, "Debe introducir su nombre de usuario", Toast.LENGTH_LONG).show();
+        } else if(email.length()==0){
+            Toast.makeText(this, "Debe introducir su dirección de correo electrónico", Toast.LENGTH_LONG).show();
+        } else if(passw.length()==0){
+            Toast.makeText(this, "Debe crear una contraseña", Toast.LENGTH_LONG).show();
+        } else if(cb==false){
+            Toast.makeText(this, "Debe aceptar Términos y Condiciones.", Toast.LENGTH_LONG).show();
         }
+
+        else{
+            try{
+                ContentValues c = new ContentValues();
+                c.put("USER", user);
+                c.put("EMAIL", email);
+                c.put("PASS", passw);
+                db.insert("PERSONA", null, c);
+                db.close();
+                Toast.makeText(this, "Registro insertado.", Toast.LENGTH_LONG).show();
+            } catch (Exception e){
+                Toast.makeText(this, "Error:"+e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+
     }
     public void AbrirDialogo(){
         Dialog dialog = new Dialog();
